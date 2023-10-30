30b8740a2cfe1a8de898300f7672e4e7b78f4521
Alex <125179260+Prodigysy@users.noreply.github.com>
2023-10-30 13:50:14 +0400

    Add files via upload

---
+++ task_6.py
@@ -0,0 +1,34 @@
+import argparse
+import concurrent.futures
+import requests
+from PIL import Image
+from io import BytesIO
+
+def download_image(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return Image.open(BytesIO(response.content))
+        else:
+            print(f"Failed to download image from {url}")
+            return None
+    except Exception as e:
+        print(f"Error while downloading image from {url}: {e}")
+        return None
+
+def main():
+    parser = argparse.ArgumentParser(description="Parallel image downloader")
+    parser.add_argument("urls", nargs="+", help="List of image URLs to download")
+
+    args = parser.parse_args()
+    urls = args.urls
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        images = list(executor.map(download_image, urls))
+
+    for idx, image in enumerate(images):
+        if image:
+            image.save(f"image_{idx}.jpg")
+
+if __name__ == "__main__":
+    main()
